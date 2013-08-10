f1945ef55008dc78634b46bbe460f1a099ee7fca
Charles Pinnix <cpinnix89@gmail.com>
2013-08-10 13:27:59 -0400

    Added new binary, need to ignore bin.

--- go/src/github.com/graffissimo/graffissimo.go
+++ go/src/github.com/graffissimo/graffissimo.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "code.google.com/p/go.net/websocket"
-    "net/http"
-    "log"
+	"code.google.com/p/go.net/websocket"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 var (
@@ -27,51 +27,51 @@ func init() {
 
 // WebSocket server to handle chat between clients
 func SockServer(ws *websocket.Conn) {
-    fmt.Println("This is this socket server.")
+	fmt.Println("This is this socket server.")
 
-    var err error
-    var clientMessage string
-    // use []byte if websocket binary type is blob or arraybuffer
-    // var clientMessage []byte
+	var err error
+	var clientMessage string
+	// use []byte if websocket binary type is blob or arraybuffer
+	// var clientMessage []byte
 
-    // cleanup on server side
-    defer func() {
-        if err = ws.Close(); err != nil {
-            log.Println("Websocket could not be closed", err.Error())
-        }
-    }()
+	// cleanup on server side
+	defer func() {
+		if err = ws.Close(); err != nil {
+			log.Println("Websocket could not be closed", err.Error())
+		}
+	}()
 
-    client := ws.Request().RemoteAddr
-    log.Println("Client connected:", client)
-    sockCli := ClientConn{ws, client}
-    ActiveClients[sockCli] = 0
-    log.Println("Number of clients connected ...", len(ActiveClients))
+	client := ws.Request().RemoteAddr
+	log.Println("Client connected:", client)
+	sockCli := ClientConn{ws, client}
+	ActiveClients[sockCli] = 0
+	log.Println("Number of clients connected ...", len(ActiveClients))
 
-    // for loop so the websocket stays open otherwise
-    // it'll close after one Receieve and Send
-    for {
-        if err = Message.Receive(ws, &clientMessage); err != nil {
-            // If we cannot Read then the connection is closed
-            log.Println("Websocket Disconnected waiting", err.Error())
-            // remove the ws client conn from our active clients
-            delete(ActiveClients, sockCli)
-            log.Println("Number of clients still connected ...", len(ActiveClients))
-            return
-        }
+	// for loop so the websocket stays open otherwise
+	// it'll close after one Receieve and Send
+	for {
+		if err = Message.Receive(ws, &clientMessage); err != nil {
+			// If we cannot Read then the connection is closed
+			log.Println("Websocket Disconnected waiting", err.Error())
+			// remove the ws client conn from our active clients
+			delete(ActiveClients, sockCli)
+			log.Println("Number of clients still connected ...", len(ActiveClients))
+			return
+		}
 
-        clientMessage = sockCli.clientIP + " Said: " + clientMessage
-        for cs, _ := range ActiveClients {
-            if err = Message.Send(cs.websocket, clientMessage); err != nil {
-                // we could not send the message to a peer
-                log.Println("Could not send message to ", cs.clientIP, err.Error())
-            }
-        }
-    }
+		clientMessage = sockCli.clientIP + " Said: " + clientMessage
+		for cs, _ := range ActiveClients {
+			if err = Message.Send(cs.websocket, clientMessage); err != nil {
+				// we could not send the message to a peer
+				log.Println("Could not send message to ", cs.clientIP, err.Error())
+			}
+		}
+	}
 }
 
 func main() {
-    err := http.ListenAndServe(":3000", nil)
-    if err != nil {
-        panic("ListenAndServe: " + err.Error())
-    }
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 }
